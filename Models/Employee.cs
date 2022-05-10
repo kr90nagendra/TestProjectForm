3470d9e9ed3005260615e0461cc5415a33b47f47
kr90nagendra <105261101+kr90nagendra@users.noreply.github.com>
2022-05-10 12:39:01 +0530

    Add project files.

---
+++ Models/Employee.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data;
+using System.ComponentModel.DataAnnotations;
+
+namespace TestProjectForm.Models
+{
+    public class Employee
+    {
+        [Required]
+        [Display(Name ="Employee First Name")]
+        public string  Empfname { get; set; }
+        [Required]
+        [Display(Name = "Employee Last Name")]
+        public string Emplname { get; set; }
+    }
+}
