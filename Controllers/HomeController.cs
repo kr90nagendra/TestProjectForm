2e8eddf374fe99b909356c95665a3b2d00632fb7
kr90nagendra <105261101+kr90nagendra@users.noreply.github.com>
2022-05-10 12:38:31 +0530

    Updated Route file
    Add project files.
    
    Add .gitignore and .gitattributes.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TestProjectForm.Models;
+using System.IO;
+
+using Newtonsoft.Json;
+
+namespace TestProjectForm.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        [HttpGet]
+        public ActionResult Postdata()
+        {
+
+          
+
+            return View();
+        }
+        [HttpPost]
+        [ActionName ("Postdata")]
+        public ActionResult Adddata(Employee emp)
+        {
+
+             List<Employee> emlist = new List<Employee>();
+
+            emlist.Add(new Employee()
+            {
+                Empfname=emp.Empfname,
+                Emplname=emp.Emplname
+               
+
+            });
+
+           
+            string path = Server.MapPath("~/app_data/db.json");
+
+
+            var jsondata = JsonConvert.SerializeObject(emlist);
+             if(ModelState.IsValid)
+            {
+                System.IO.File.AppendAllText(path, jsondata);
+
+                ViewBag.cnf = "Employee details added successfully";
+                ModelState.Clear();
+              
+            }
+
+         
+           
+
+            
+
+
+            return View();
+        }
+
+       
+    }
+}
